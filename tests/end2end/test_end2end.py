ed7598d9214d7c99b88da18b022bf0f21d04e704
Artur Costa-Pazo <acosta@alicebiometrics.com>
2020-04-15 13:22:30 +0200

    Solve lint warnings

--- tests/end2end/test_end2end.py
+++ tests/end2end/test_end2end.py
@@ -14,9 +14,7 @@ def base_url(variables):
     not os.environ.get("END2END_TEST"),
     reason="To run end2end test, please define END2END_TEST envar",
 )
-def test_end2end(
-        base_url, given_any_title, given_any_description
-):
+def test_end2end(base_url, given_any_title, given_any_description):
     response = requests.get(f"{base_url}/healthcheck")
     assert response.status_code == 200
 
