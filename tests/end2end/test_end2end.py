9a10eb35d3725d7d7d892c07c660aa586f5ed026
Artur Costa-Pazo <acosta@alicebiometrics.com>
2020-04-15 08:47:42 +0200

    Add new assertions on end2end test

--- tests/end2end/test_end2end.py
+++ tests/end2end/test_end2end.py
@@ -17,7 +17,16 @@ def base_url(variables):
 def test_end2end(
         base_url, given_any_title, given_any_description
 ):
+    response = requests.get(f"{base_url}/healthcheck")
+    assert response.status_code == 200
+
+    response = requests.get(f"{base_url}/task/invalid_id")
+    assert response.status_code == 404
+
     data = {"title": given_any_title, "description": given_any_description}
     response = requests.post(f"{base_url}/task", json=data)
+    assert response.status_code == 200
+    task_id = response.json()["task_id"]
 
+    response = requests.get(f"{base_url}/task/{task_id}")
     assert response.status_code == 200
