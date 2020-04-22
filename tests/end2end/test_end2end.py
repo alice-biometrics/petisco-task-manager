c49adc10b3112dd77bc760716b4d59c5d9170d5e
Artur Costa-Pazo <acosta@alicebiometrics.com>
2020-04-22 11:01:58 +0200

    Update petisco version (v0.21.0) and lume version (v0.2.9). This will speed up our development flow :rocket:

--- tests/end2end/test_end2end.py
+++ tests/end2end/test_end2end.py
@@ -15,6 +15,7 @@ def base_url(variables):
     reason="To run end2end test, please define END2END_TEST envar",
 )
 def test_end2end(base_url, given_any_title, given_any_description):
+
     response = requests.get(f"{base_url}/healthcheck")
     assert response.status_code == 200
 
