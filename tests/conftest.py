7b21faa6a8439a5446ae79d3cab1f3f231541168
Artur Costa-Pazo <acosta@alicebiometrics.com>
2020-04-22 11:55:32 +0200

    mend

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,39 +2,9 @@ import os
 import pytest
 
 from tests.modules.fixtures import *
+from petisco.fixtures import *
 
 if not os.environ.get("END2END_TEST"):
-    from sqlalchemy import create_engine
-    from petisco import SqlAlchemyPersistence, Petisco
     from taskmanager import petisco_setup
+
     petisco_setup()
-    app = Petisco.get_instance().get_app()
-
-
-@pytest.fixture
-def client():
-    with app.app.test_client() as c:
-        yield c
-
-
-@pytest.fixture
-def database():
-    """
-    database = os.environ.get("SQL_DATABASE", "taskmanager")
-    database = f"{database}_test"
-    connection = f"sqlite:///{database}"
-    engine = create_engine(connection)
-
-    Base = SqlAlchemyPersistence.get_instance().base
-    Session = SqlAlchemyPersistence.get_instance().session
-
-    Base.metadata.create_all(engine)
-
-    yield
-
-    session = Session()
-    session.rollback()
-    session.close()
-    Base.metadata.drop_all(bind=engine)
-    os.remove(database)
-    """
--- tests/modules/tasks/create/acceptance/test_post_task.py
+++ tests/modules/tasks/create/acceptance/test_post_task.py
@@ -5,11 +5,13 @@ import pytest
 
 @pytest.mark.acceptance
 def test_should_return_a_200_when_post_task(
-    client, database, given_any_title, given_any_description
+    petisco_client, given_any_title, given_any_description
 ):
     headers = {"Content-type": "application/json", "Accept": "text/plain"}
     data = {"title": given_any_title, "description": given_any_description}
-    response = client.post("/taskmanager/task", data=json.dumps(data), headers=headers)
+    response = petisco_client.post(
+        "/taskmanager/task", data=json.dumps(data), headers=headers
+    )
 
     assert response.status_code == 200
     assert "task_id" in response.json
@@ -19,44 +21,52 @@ def test_should_return_a_200_when_post_task(
 
 @pytest.mark.acceptance
 def test_should_return_a_405_when_post_task_with_no_data(
-    client, database, given_any_title, given_any_description
+    petisco_client, given_any_title, given_any_description
 ):
     headers = {"Content-type": "application/json", "Accept": "text/plain"}
     data = {}
-    response = client.post("/taskmanager/task", data=json.dumps(data), headers=headers)
+    response = petisco_client.post(
+        "/taskmanager/task", data=json.dumps(data), headers=headers
+    )
 
     assert response.status_code == 400
 
 
 @pytest.mark.acceptance
 def test_should_return_a_400_when_post_task_with_no_title(
-    client, database, given_any_description
+    petisco_client, given_any_description
 ):
     headers = {"Content-type": "application/json", "Accept": "text/plain"}
     data = {"description": given_any_description}
-    response = client.post("/taskmanager/task", data=json.dumps(data), headers=headers)
+    response = petisco_client.post(
+        "/taskmanager/task", data=json.dumps(data), headers=headers
+    )
 
     assert response.status_code == 400
 
 
 @pytest.mark.acceptance
 def test_should_return_a_400_when_post_task_with_no_description(
-    client, given_any_title
+    petisco_client, given_any_title
 ):
     headers = {"Content-type": "application/json", "Accept": "text/plain"}
     data = {"title": given_any_title}
-    response = client.post("/taskmanager/task", data=json.dumps(data), headers=headers)
+    response = petisco_client.post(
+        "/taskmanager/task", data=json.dumps(data), headers=headers
+    )
 
     assert response.status_code == 400
 
 
 @pytest.mark.acceptance
 def test_should_return_a_405_when_post_task_with_title_exceeds_length(
-    client, given_any_title, given_any_description
+    petisco_client, given_any_title, given_any_description
 ):
     headers = {"Content-type": "application/json", "Accept": "text/plain"}
     data = {"title": given_any_title * 20, "description": given_any_description}
-    response = client.post("/taskmanager/task", data=json.dumps(data), headers=headers)
+    response = petisco_client.post(
+        "/taskmanager/task", data=json.dumps(data), headers=headers
+    )
     assert response.status_code == 405
     assert response.json == {
         "error": {
@@ -68,11 +78,13 @@ def test_should_return_a_405_when_post_task_with_title_exceeds_length(
 
 @pytest.mark.acceptance
 def test_should_return_a_405_when_post_task_with_description_exceeds_length(
-    client, given_any_title, given_any_description
+    petisco_client, given_any_title, given_any_description
 ):
     headers = {"Content-type": "application/json", "Accept": "text/plain"}
     data = {"title": given_any_title, "description": given_any_description * 200}
-    response = client.post("/taskmanager/task", data=json.dumps(data), headers=headers)
+    response = petisco_client.post(
+        "/taskmanager/task", data=json.dumps(data), headers=headers
+    )
     assert response.status_code == 405
     assert response.json == {
         "error": {
