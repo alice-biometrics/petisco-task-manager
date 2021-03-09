bd4e3d414fbac6256904e8d3c5807ed2a226ff12
Francisco Rodeño <frodeno@alicebiometrics.com>
2021-03-09 08:54:22 +0100

    Fixed the root path in tests configuration file

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,14 +5,14 @@ from tests.modules.fixtures import *
 from petisco.fixtures import *
 
 
+ROOT_PATH = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+
+
 def test_persistence_setup():
-    ROOT_PATH = (
-        "/Users/acosta/Software/alice/open_source/petisco-task-manager/taskmanager"
-    )
     sql_database = SqliteDatabase(
         name="taskmanager",
         connection=SqliteConnection.create("sqlite", "tasmanager.db"),
-        model_filename=f"{ROOT_PATH}/petisco.sql.models.yml",
+        model_filename=f"{ROOT_PATH}/taskmanager/petisco.sql.models.yml",
     )
     persistence = Persistence()
     persistence.add(sql_database)
